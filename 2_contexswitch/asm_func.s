23930ae6a0e07fa255ce6739cf0fa09eaad711d4
GOGOGOGOGOGOG <gogo0929052207@gmail.com>
2019-06-12 17:45:55 +0800

    First commit

--- 2_contexswitch/asm_func.s
+++ 2_contexswitch/asm_func.s
@@ -20,23 +20,24 @@ start_user:
 .global systick_handler
 systick_handler:
 	//save lr (EXC_RETURN) to main stack
-	??????
+	push {lr} 
 
 	//save r4-r11 to user stack
-	mrs	r0,	psp
-	stmdb	r0!,	{r4-r11}
+	mrs	r0,	psp // 把psp的指標處存在r0 ，push的概念是 先下降 再把該處的資料存起來
+	stmdb	r0!,	{r4-r11} // store multiple decrease before  //透過r0的指標來把r4~r11的值存起來 ！ ＝>r0會被更新成最後一筆位址 若不加驚嘆號 則r0便不會改變
 
 	//pass psp of curr task by r0 and get psp of the next task
 	bl	sw_task
 	//psp of the next task is now in r0
 
+
 	//restore r4~r11 from stack of the next task
-	??????
+    ldmia r0!,{r4-r11} // load multiple decrease after 
 
 	//modify psp
-	??????
+	msr psp,r0
 
 	//restore lr (EXC_RETURN)
-	??????
+	pop {lr} 
 
 	bx	lr
